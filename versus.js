49e6084484997ebb4917007bd729d22069d8b540
Adam Rana <adamek.rana@gmail.com>
2024-01-07 15:23:49 +0100

    changed everzthing

--- versus.js
+++ versus.js
@@ -1,140 +1,73 @@
-const addButton = document.querySelector("#add")
+const cookieButton = document.querySelector("#cookie")
 const valueText = document.querySelector("#value")
-
-addButton.addEventListener("click", add)
-
-patchData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json", {versus:1}).then((data) => {
-        console.log(data); 
-         
-        
-    });
-
-
 let value = 1
 
+cookieButton.addEventListener("click", add)
 
+patchData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json", { versus: value }).then((data) => {
 
+});
 
 function add() {
-    console.log("hoj")
-value++
-    patchData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json", {versus:value}).then((data) => {
-        console.log(data); 
-         
-        
-    });
+  value++
+  patchData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json", { versus: value }).then((data) => {
+
+  });
 }
 
+function scores() {
+
+  getData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json").then((data) => {
+    document.querySelector('#firstscore').innerHTML = data.versus
+    document.querySelector('#secondscore').innerHTML = data.versus2
+    document.querySelector('#thirdscore').innerHTML = data.versus3
+    document.querySelector('#forthscore').innerHTML = data.versus4
+    document.querySelector('#fifthscore').innerHTML = data.versus5
+  });
 
 
+}
 
-function chngething() {
+setInterval(() => {
+  scores()
+  sortleadboard()
+}, 10)
 
-    getData("https://awesomeapp-f2b19-default-rtdb.asia-southeast1.firebasedatabase.app/.json").then((data) => {
-        document.querySelector('#score').innerHTML = value
-        document.querySelector('#anoscore').innerHTML = data.versus2
-        document.querySelector('#nescore').innerHTML = data.versus3
-        document.querySelector('#nenescore').innerHTML = data.versus4
-        document.querySelector('#anoanoscore').innerHTML = data.versus5
-    
-        
-    });
-    
-    
-    }
-    
-    setInterval(chngething, 100)
-    
-    function sortleadboard() {
-        let elements = []
-        let container = document.querySelector('#container')
-        // Add each row to the array
-        container.querySelectorAll('.row').forEach(el => elements.push(el))
-        // Clear the container
-        container.innerHTML = ''
-        // Sort the array from highest to lowest
-        elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
-        // Put the elements back into the container
-        elements.forEach(e => container.appendChild(e))
-      }
-      
-    setInterval(sortleadboard, 100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function sortleadboard() {
+  let elements = []
+  let container = document.querySelector('#container')
+  container.querySelectorAll('.row').forEach(el => elements.push(el))
+  container.innerHTML = ''
+  elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
+  elements.forEach(e => container.appendChild(e))
+}
 
 
 
 
 async function patchData(url = "", data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: "PATCH", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "PATCH", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
 
 
 
 async function getData(url = "") {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      }
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
-  
